f60786a160e3ce04144d4a3dc8e3820a611462a8
IslamKalykov <islamkalykov1@gmail.com>
2024-01-26 12:01:39 +0600

    Initial commit

--- playground_booking/playground_booking/settings.py
+++ playground_booking/playground_booking/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6x^f9ork8kam+ni1vsk5ll8sg1ly6ce&vis+35^@(am+04&0y8"
+SECRET_KEY = os.getenv('DJANGO_TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
