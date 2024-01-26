71cbb669f031699321fc2f62b9bc022686237f63
IslamKalykov <islamkalykov1@gmail.com>
2024-01-26 13:05:15 +0600

    Created models and added App booking

---
+++ playground_booking/booking/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils import timezone
+
+class CustomUser(AbstractUser):
+    ROLES = (
+        ('user', 'User'),
+        ('admin', 'Admin'),
+    )
+    groups = models.ManyToManyField(Group, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_permissions')
+    phone_number = models.CharField(max_length=15, blank=False)
+    role = models.CharField(max_length=10, choices=ROLES, default='user')
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='images/user_images/', null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+class Playground(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=True)
+    admin = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, blank=False, null=True)
+    phone_number = models.CharField(max_length=15, blank=False, null=True)
+    bookings = models.ForeignKey('Booking', null=True, blank=True, on_delete=models.CASCADE, related_name='playground_bookings')
+    price = models.FloatField(default=0, blank=False)
+    width = models.FloatField(blank=False, null=True)
+    height = models.FloatField(blank=False, null=True)
+    reservation_status = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='images/playground_images/', null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.admin.username} - {self.id}'
+
+
+
+class Booking(models.Model):
+    playground = models.ForeignKey(Playground, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    is_confirmed = models.BooleanField(default=False)
+    admin_status = models.CharField(max_length=15, choices=(('pending', 'Pending'), ('approved', 'Approved'), ('rejected', 'Rejected')), default='pending')
+    total_price = models.FloatField(default=0, blank=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.playground.name} - {self.id}'
